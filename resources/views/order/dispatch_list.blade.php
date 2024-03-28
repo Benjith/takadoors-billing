c69421678fdb1a4c7dee3c503d3543577ea8886b
shirin <fathimashirin866@gmail.com>
2024-03-28 18:42:46 +0300

    order steps

---
+++ resources/views/order/dispatch_list.blade.php
@@ -0,0 +1,90 @@
+
+@extends('layouts.outer')
+@section('content') 
+   <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          
+          <div class="row">
+            
+          <form method="POST" action="{{ route('dispatch_search')}}">
+                        @csrf
+                  <div class="input-group">
+                    <div class="col-xs-6">
+                        <input type="date" id="fromDate" name="fromdate" value="<?php if(isset($from_date)) echo $from_date; ?>" class="form-control" placeholder="From Date" aria-label="search" aria-describedby="search">                      
+                    </div>  
+                    <div class="col-xs-6">
+                        <input type="date" id="toDate" name="todate" value="<?php if(isset($to_date)) echo $to_date; ?>" class="form-control" placeholder="To Date" aria-label="search" aria-describedby="search">
+                    </div> 
+                    <div class="col-xs-6">
+                         <input type="text" id="code" name="code" value="<?php if(isset($code)) echo $code; ?>" class="form-control" placeholder="Enter Code" aria-label="search" aria-describedby="search">                          
+                    </div>
+                    <div class="col-xs-6 agent-input">
+                        <button class="btn btn-primary mt-2 mt-xl-0">Submit</button>    
+                    </div>
+                  </div>                    
+        </form>
+</div>
+          <div class="status_message"></div>
+          @if(Session::has('error'))
+          <div class="alert-danger flash-message" >  <span> {{ Session::get('error') }} </span>   </div>
+          @endif
+          @if (Session::has('success'))
+          <div class="alert-success flash-message" >  <span> {{ Session::get('success') }} </span>   </div>
+          @endif
+          <div class="row">
+            <div class="col-md-12 stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <p class="card-title">DISPATCH REPORT</p>
+                  <div class="table-responsive">
+                    <table id="dispatch_table" class="table">
+                      <thead>
+                        <tr>
+                            <th>SI.No.</th>
+                            <th>Thickness</th>
+                            <th>Length</th>
+                            <th>Width</th>
+                            <th>Quantity</th>
+                            <th>Design</th>
+                            <th> PVC Model  </th>
+                            <th> Code </th>
+                            <th>Remarks</th>
+                        </tr>
+                      </thead>
+                      <tbody class="cus_results">
+                      <?php $count=1;?>
+                        @foreach ($orders as $order)
+                        <tr>
+                        <td style="width:5%">{{$order->serial_no}}</td>
+                            <td>{{$order->thickness}}</td>
+                            <td>{{$order->length}}</td>
+                            <td>{{$order->width}}</td>
+                            <td>{{$order->quantity}}</td>
+                            <td>{{$order->design}}</td>
+                            <td>{{$order->frame}}</td>
+                            <td>{{$order->code}}</td>
+                            <td>{{$order->remarks}}</td>
+                        </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                {{ $orders->links('pagination::bootstrap-4') }}
+                  </div>
+                </div>
+          
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+@endsection
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+$(document).ready(function() {
+    $('#dispatch_table').DataTable({
+        "order": [] // Disable automatic sorting
+    });
+});
+</script>
---
+++ resources/views/order/driver_order_pdf.blade.php
@@ -0,0 +1,45 @@
+ 
+<div class="container">  
+<h3 style="text-align: center;" class="heading-section">DISPATCH LIST</h3>
+    <br/> 
+    <p><b>Driver Name:</b> {{$driverName}} </p>
+    <table style="width: 100%; background: #f7f7f7;">  
+        <tr>  
+        <th>SI.No.</th>
+                            <th>Thick</th>
+                            <th>Length</th>
+                            <th>Width</th>
+                            <th>No.s</th>
+                            <th>Design</th>
+                            <th>PVC Model </th>
+                            <th>Code</th>
+                            <th>Remarks</th>
+        </tr> <?php $quantity = 0; ?> 
+        @foreach ($orders as $key => $order)
+        <?php $quantity = $quantity+$order->quantity;?>
+        <tr>  
+        <td>{{$order->serial_no}}</td>
+        <td>{{$order->thickness}}</td>
+                            <td>{{$order->length}}</td>
+                            <td>{{$order->width}}</td>
+                            <td>{{$order->quantity}}</td>
+                            <td>{{$order->design}}</td>
+                            <td>{{$order->frame}}</td>
+                            <td>{{$order->code}}</td>
+                            <td>{{strtoupper($order->remarks)}}</td>
+
+        </tr> 
+        @endforeach 
+    </table>
+   <p><b>Total Quantity : </b><?php if(isset($quantity)) echo $quantity; ?></p>
+
+</div>  
+<style type="text/css">  
+    table, td,tr,th{  
+        border:1px solid black; 
+        border-collapse: collapse;
+    }
+    td{
+        text-align: center;
+    }  
+</style> 
